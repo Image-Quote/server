113e0c3f0db6623632a104f1836429d62bea5acc
vinesto <oqvinesto@gmail.com>
2018-09-13 22:24:52 +0700

    modify route upload with vision api

--- routes/images.js
+++ routes/images.js
@@ -1,9 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var images = require('../helpers/images')
+var multer = require('multer')
 var {uploadImage, getAllImage} = require('../controllers/images')
 
 /* GET users listing. */
 router.get('/', getAllImage)
-router.post('/', uploadImage)
+router.post('/',images.multer.single('image'),images.sendUploadToGCS,uploadImage)
 
 module.exports = router;
